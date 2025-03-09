4bb31c6200157448e2673c7ba319650d770086ad
Quackers <quackadoobers@gmail.com>
2025-03-09 16:09:28 +0800

    Cleanup: Data source variable named "material"

--- ui.py
+++ ui.py
@@ -75,18 +75,18 @@ class EMP_PT_UL_PASSES(UIList):
 class EMP_OT_ADD_PASS(Operator): 
     bl_idname = "my_list.new_item" 
     bl_label = "Add Property" 
-    bl_description = "Add a custom object property for all users of the current material"
+    bl_description = "Add a custom object property for all users of the current data"
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context): 
         collection = get_collection_property(context)
         collection.add()
         
-        material = get_active_scene(context)
+        data = get_active_scene(context)
         max_index = len(collection) - 1
 
-        intended_index = clamp(material.list_index + 1, lower=0, upper=max_index)
-        material.list_index = intended_index
+        intended_index = clamp(data.list_index + 1, lower=0, upper=max_index)
+        data.list_index = intended_index
         collection.move(max_index, intended_index)
 
         return {'FINISHED'}
@@ -95,22 +95,20 @@ class EMP_OT_ADD_PASS(Operator):
 class EMP_OT_REMOVE_PASS(Operator): 
     bl_idname = "my_list.delete_item" 
     bl_label = "Remove Property" 
-    bl_description = "Remove selected property for all users of the current material"
+    bl_description = "Remove selected property for all users of the current data"
     bl_options = {"REGISTER", "UNDO"}
 
     @classmethod 
-    def poll(cls, context): 
-        prop = get_collection_property(context)
-        
+    def poll(cls, context):
         return get_active_scene(context)
 
     def execute(self, context):
-        material = get_active_scene(context)
+        data = get_active_scene(context)
         prop = get_collection_property(context)
-        index = material.list_index
+        index = data.list_index
 
         prop.remove(index) 
-        material.list_index = min(max(0, index - 1), len(prop) - 1) 
+        data.list_index = min(max(0, index - 1), len(prop) - 1) 
         return{'FINISHED'}
 
 
@@ -139,20 +137,20 @@ class EMP_OT_MOVE_PASS(Operator):
     def poll(cls, context): 
         return get_active_scene(context)
 
-    def move_index(self, collection, material, index): 
+    def move_index(self, collection, data, index): 
         list_length = len(collection) - 1 # (index starts at 0) 
         new_index = index + (-1 if self.direction == 'UP' else 1)
-        material.list_index = max(0, min(new_index, list_length)) 
+        data.list_index = max(0, min(new_index, list_length)) 
 
     def execute(self, context): 
-        material = get_active_scene(context)
-        index = material.list_index
+        data = get_active_scene(context)
+        index = data.list_index
         my_list = get_collection_property(context)
 
         neighbor = index + (-1 if self.direction == 'UP' else 1) 
         my_list.move(neighbor, index) 
         
-        self.move_index(my_list, material, index) 
+        self.move_index(my_list, data, index) 
         return{'FINISHED'}
     
 
