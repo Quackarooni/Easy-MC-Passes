cdbd02d115b616a595ac040232ecc115ac5773af
Quackers <quackadoobers@gmail.com>
2025-03-10 21:46:16 +0800

    Utils: Add `fetch_user_preferences` function

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import bpy
 
 
+def fetch_user_preferences(attr_id=None):
+    prefs = bpy.context.preferences.addons[__package__].preferences
+
+    if attr_id is None:
+        return prefs
+    else:
+        return getattr(prefs, attr_id)
+
+
 def get_enabled_passes(collection):
     for render_pass in collection:
         if render_pass.render:
