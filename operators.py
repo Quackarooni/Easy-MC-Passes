477dfa2bc40899b229057d2abe9f238953962520
Quackers <quackadoobers@gmail.com>
2025-03-12 20:31:21 +0800

    Export: Initiate rendering when operator is executed

--- operators.py
+++ operators.py
@@ -32,10 +32,10 @@ class EMP_OT_EXPORT_PASSES(Operator):
 
         main_scene = create_scene(scene, "EMP_Export_Passes", clear_tree=True)
         init_main_passes_scene(main_scene, passes=main_passes)
-        
+
         tree = main_scene.node_tree
         output_node = add_node(tree, "CompositorNodeOutputFile", name="File Output (Images)", base_path=export_path, width=360, location=(500.0, 450.0))
-        exr_output_node = add_node(tree, "CompositorNodeOutputFile", name="File Output (EXR)", base_path=export_path, width=360, location=(500.0, 160.0))
+        exr_output_node = add_node(tree, "CompositorNodeOutputFile", name="File Output (EXR)", base_path=export_path + "Multilayer", width=360, location=(500.0, 160.0))
         exr_output_node.format.file_format = "OPEN_EXR_MULTILAYER"
 
         main_passes_node = add_node(tree, "CompositorNodeRLayers", name="Main Passes", location=(0.0, 450.0))
@@ -61,6 +61,7 @@ class EMP_OT_EXPORT_PASSES(Operator):
         for render_pass in passes:
             link_pass_sockets(tree, render_pass.name)
 
+        bpy.ops.render.render('INVOKE_SCREEN', scene=main_scene.name)
         return {'FINISHED'}
 
 
