11fcc1a7c6dee9aa39c23e467efa88f8494dc665
Quackers <quackadoobers@gmail.com>
2025-03-13 14:22:02 +0800

    Refactor: Remove unique name calculation ni `EMPRenderPass`

--- prefs.py
+++ prefs.py
@@ -43,51 +43,11 @@ def setDefaultCollectionValue():
 
 
 class EMPRenderPass(PropertyGroup):
-    def parent_collection(self):
-        # this gets the collection that the element is in
-        path = self.path_from_id()
-        match = re.match('(.*)\[\d*\]', path)
-        parent = self.id_data
-        try:
-            coll_path = match.group(1)
-        except AttributeError:
-            raise TypeError("Property not element in a collection.") 
-        else:
-            return parent.path_resolve(coll_path)
-
     def __repr__(self):
         return f"bpy.data.scenes['{bpy.context.scene.name}'].{self.__class__.__name__}['{self.name}']"
 
-    @staticmethod
-    def unduped_name(name):
-        unduped_name, *_ = re.split("\.\d+$", name)
-        return unduped_name
-
-    def make_name_unique(self, name):
-        collection = self.parent_collection()
-        counter = 1
-        names = set(i.name for i in collection)
-
-        stem = self.unduped_name(name)
-
-        counts = Counter(i.name for i in collection)
-        should_change_name = (counts[name] > 1)
-        
-        if should_change_name:
-            while name in names:
-                name = f"{stem}.{counter:03d}"
-                counter += 1
-
-        return name
-
-    def set_unique_name(self, context):
-        self["name"] = self.make_name_unique(self.name)
-
-    name: StringProperty(name="Name", default="Default", update=set_unique_name)
+    name: StringProperty(name="Name", default="Default")
     render: BoolProperty(name="Render", default=True)
-
-    def initialize_name(self):
-        self.name = self.name
 
     def draw(self, layout):
         if self.name in {"Shading", "Shadow"}:
